f3017a6b36a8752837064571bd568e5f367eeacf
Charlyemex <charlyemex@gmail.com>
2023-05-25 00:28:06 -0700

    my update

--- init_info.c
+++ init_info.c
@@ -13,13 +13,13 @@ void init_info(info_t *info)
 /**
  * reset_info - It resets the  info_t struct.
  * @info: Then struct address.
- * @av: The argument vector
+ * @arv: The argument vector
  */
-void set_info(info_t *info, char **av)
+void reset_info(info_t *info, char **arv)
 {
-	int i = 0;
+	int j = 0;
 
-	info->fname = av[0];
+	info->fname = arv[0];
 	if (info->arg)
 	{
 		info->argv = strtow(info->arg, " \t");
@@ -32,26 +32,25 @@ void set_info(info_t *info, char **av)
 				info->argv[1] = NULL;
 			}
 		}
-		for (i = 0; info->argv && info->argv[i]; i++)
+		for (j = 0; info->argv && info->argv[j]; j++)
 			;
-		info->argc = i;
+		info->argc = j;
 
 		replace_alias(info);
 		replace_vars(info);
 	}
 }
-
 /**
- * free_info - frees info_t struct fields
- * @info: struct address
- * @all: true if freeing all fields
+ * frees_info - It sets free info_t struct file.
+ * @info: The Struct address.
+ * @al: Show true if setting all files free.
  */
-void free_info(info_t *info, int all)
+void frees_info(info_t *info, int al)
 {
 	ffree(info->argv);
 	info->argv = NULL;
 	info->path = NULL;
-	if (all)
+	if (al)
 	{
 		if (!info->cmd_buf)
 			free(info->arg);
@@ -63,7 +62,7 @@ void free_info(info_t *info, int all)
 			free_list(&(info->alias));
 		ffree(info->environ);
 			info->environ = NULL;
-		bfree((void **)info->cmd_buf);
+		ffree((char **)info->cmd_buf);
 		if (info->readfd > 2)
 			close(info->readfd);
 		_putchar(BUF_FLUSH);
