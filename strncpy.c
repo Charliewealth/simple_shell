8d0938ea5ba744e81bfbb3438d261dd57d4919e7
Charlyemex <charlyemex@gmail.com>
2023-05-25 08:08:26 -0700

    my update

--- strncpy.c
+++ strncpy.c
@@ -56,10 +56,10 @@ char *_strncat(char *dest, char *src, int n)
 }
 
 /**
- **_strchr - locates a character in a string
- *@s: the string to be parsed
- *@c: the character to look for
- *Return: (s) a pointer to the memory area s
+ **_strchr - It finds a char in a string.
+ *@s: The string to be searched.
+ *@c: The char to look for.
+ *Return: pointer to the memory area s
  */
 char *_strchr(char *s, char c)
 {
@@ -70,4 +70,3 @@ char *_strchr(char *s, char c)
 
 	return (NULL);
 }
-
